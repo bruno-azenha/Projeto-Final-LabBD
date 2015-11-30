2a47218bf3f1fe971d5bca78b3e4a9e98d2838ff
- <azenha.bruno@gmail.com>
2015-11-30 19:13:11 -0200

    Adiciona query de pedidos

--- Proj_LabDB/settings.py
+++ Proj_LabDB/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website'
+    'bootstrapform',
+    'website',
 )
 
 MIDDLEWARE_CLASSES = (
--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 
 from django.db import connection
 
+from .forms import *
 from .models import *
 
 def executeSQL(sql_string):
@@ -21,9 +22,24 @@ def LandingPage(request):
 	)
 
 def Pedidos(request):
+	if request.method == 'POST':
+		print("ok")
+	else:
+		query = "SELECT P.CODIGO, ( C.PRIMEIRONOME || ' ' || C.NOMEDOMEIO || ' ' || C.SOBRENOME ) as cliente, P.DTPEDIDO FROM PEDIDO P INNER JOIN CLIENTE C ON P.CODIGOCLIENTE = C.CODIGO WHERE P.CODIGOVENDEDOR=279"
+		pedidos = executeSQL(query)
+		pedidos_list = []
+		for p in pedidos:
+			pedido_id = p[0]
+			pedido_cliente = p[1]
+			pedido_data = p[2]
+			print (pedido_cliente)
+			pedidos_list.append({"id": pedido_id, "cliente": pedido_cliente, "data": pedido_data})
+
+		form = PedidoForm()
+
 	return render_to_response(
 		'pedidos.html',
-		{},
+		{'form': form, 'pedidos_list': pedidos_list},
 		context_instance=RequestContext(request)
 	)
 
